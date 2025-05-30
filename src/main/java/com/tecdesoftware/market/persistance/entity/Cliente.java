8530f1b1c5f6f2576b7e7cecee9270ad952acf60
Alvaro Novelo <alvaronovelo19@gmail.com>
2025-05-30 10:53:58 -0600

    All Entities Created

---
+++ src/main/java/com/tecdesoftware/market/persistance/entity/Cliente.java
@@ -0,0 +1,68 @@
+package com.tecdesoftware.market.persistance.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name= "clientes")
+public class Cliente {
+
+    @Id
+    // Ejemplo que no lleva GeneratedValue
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private Long telefono;
+
+    @Column(name= "correo electronico")
+    private String correoElectronico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
